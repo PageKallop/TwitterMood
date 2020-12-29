fe9dfc92205c67e8e0ea289d6a00519294aa4ac9
PageKallop <PageMarie78@gmail.com>
2020-12-29 14:27:27 -0600

    Setup UI Connected UI outlets

--- TwitterMood/ViewController.swift
+++ TwitterMood/ViewController.swift
@@ -9,10 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var sentimentLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func predictPressed(_ sender: UIButton) {
+    }
+    
 
 
 }
